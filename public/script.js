65a3d76edef6ed3f2db575bbdc14b0bbc3e2b77a
Trever Cullen <trevercullen@gmail.com>
2016-07-26 19:25:56 -0700

    full item clickable

--- public/script.js
+++ public/script.js
@@ -50,8 +50,8 @@ function login(){
 	var password = document.getElementById('password').value;
 	firebase.auth().signInWithEmailAndPassword(email, password).then(function(){
 		show_box('Login Successful', 0);
-		$('#login').hide(400);
-		$('#loginbuttons').hide(400);
+		$('#login').hide(600);
+		$('#loginbuttons').hide(600);
 	}, function(error){
 		var errorCode = error.code;
 		var errorMessage = error.message;
